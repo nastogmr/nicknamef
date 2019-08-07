995f9544b4a68dc9f47be9320e7d08b32a0858dc
nastogmr <51057724+nastogmr@users.noreply.github.com>
2019-08-07 23:20:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,11 @@
 bot.on("ready", () => {
   let guild = bot.guilds.get('607137367119364097'); // ايدي ال سيرفر
   setInterval(function() {
-    guild.members.get('595653353783033857').setNickname('⁹').then(() => { // الايدي مالتك
+    guild.members.get('595653353783033857').setNickname('⁹⁹⁹').then(() => { // الايدي مالتك
       setTimeout(() => {
         guild.members.get('595653353783033857').setNickname('⁹⁹').then(() => { // الايدي مالتك
           setTimeout(() => {
-            guild.members.get('595653353783033857').setNickname('⁹⁹⁹') // الايدي مالتك
+            guild.members.get('595653353783033857').setNickname('⁹') // الايدي مالتك
           }, 2000); // هذا الوقت الي يتغير فيه لاول مره
         });
       }, 4000); // هذا الوقت الي يتغير فيه لثاني مره
